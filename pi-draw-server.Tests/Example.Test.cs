3a3831f312989fce08dfbd9cf477a8db1cdf906a
Thomas Spradling <thomasspradling13@gmail.com>
2023-07-07 20:01:48 -0700

    feat: Initial setup

---
+++ pi-draw-server.Tests/Example.Test.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace pi_draw_server.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            string test = "test";
+            Assert.Equal("test", test);
+        }
+    }
+}
